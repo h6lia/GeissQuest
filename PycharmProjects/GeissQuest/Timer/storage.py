31eefc1c3cd65bb4d63eaa94a738b811b9d2d4e9
h6lia <h.ing@gmx.de>
2026-03-06 13:26:16 +0100

    cleanup 1

--- PycharmProjects/GeissQuest/Timer/storage.py
+++ PycharmProjects/GeissQuest/Timer/storage.py
@@ -1,13 +1,13 @@
 import json
 
-FILE_NAME = "data.json"
+FILE_NAME = "../data.json"
 
 def load_data():
     try:
         with open(FILE_NAME, "r") as f:
             return json.load(f)
     except FileNotFoundError:
-        return {"total_minutes": 0}
+        return {"total_minutes": 0, "total_clover": 0, "last_reset": ""}
 
 
 def save_data(data):
